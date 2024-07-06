dab93afdfaf1928a5edc7ffe534ffa8654895652
Farrel Jordan <farreljordan80@gmail.com>
2024-07-06 19:09:55 +0700

    update tls cert

--- server/main.go
+++ server/main.go
@@ -20,8 +20,8 @@ func main() {
 func run(ctx context.Context) (err error) {
 	addr := flag.String("addr", ":4443", "HTTPS server address")
 	//server deploy env
-	cert := flag.String("tls-cert", "/etc/letsencrypt/live/<domain name>/fullchain.pem", "TLS certificate file path")
-	key := flag.String("tls-key", "/etc/letsencrypt/live/<domain name>/privkey.pem", "TLS certificate file path")
+	cert := flag.String("tls-cert", "/etc/letsencrypt/live/dickyarian.blue/fullchain.pem", "TLS certificate file path")
+	key := flag.String("tls-key", "/etc/letsencrypt/live/dickyarian.blue/privkey.pem", "TLS certificate file path")
 
 	//local env
 	//cert := flag.String("tls-cert", "../cert/localhost.crt", "TLS certificate file path")
